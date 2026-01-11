7bd131476bcc7a4b7198816efd0bc540b4f225cf
veerooj <veronica.ordas@live.u-tad.com>
2026-01-11 19:20:34 +0100

    save 2

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int bulletDamage = 1;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTrigger
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint((Input.mousePosition));
+        Vector3 shootDirection = (mousePosition - transform.position).normalized;
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(shootDirection.x, shootDirection.y) * bulletSpeed;
+    }
+}
